0473508c6710fe7591498636e6021c62e1bdfca9
missnote <missnote.cn@gmail.com>
2016-05-22 00:56:50 +0800

    anthor change

--- init.el
+++ init.el
@@ -1,72 +1,24 @@
-
 ;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want (setq  initial-frame-alist (quote ((fullscreen . maximized))))it,
+;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 
-(when (>= emacs-major-version 24)
-    (require 'package)
-    (package-initialize)
-    (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-    )
+(package-initialize)
 
-(require 'cl)
+(add-to-list 'load-path "~/.emacs.d/lisp")
 
-;;add whatever packages you want here
-(defvar zilongshanren/packages '(
-				   company
-				   monokai-theme
-				   hungry-delete
-				   swiper
-				   counsel
-				   smartparens
-				   js2-mode
-				   nodejs-repl
-				   exec-path-from-shell
-				   
-				   )  "Default packages")
+(require 'init-packages)
 
-(setq package-selected-packages zilongshanren/packages)
+(setq ring-bell-function 'ignore)
 
-(defun zilongshanren/packages-installed-p ()
-    (loop for pkg in zilongshanren/packages
-          when (not (package-installed-p pkg)) do (return nil)
-          finally (return t)))
-
-(unless (zilongshanren/packages-installed-p)
-    (message "%s" "Refreshing package database...")
-    (package-refresh-contents)
-    (dolist (pkg zilongshanren/packages)
-      (when (not (package-installed-p pkg))
-        (package-install pkg))))
-
-;; let emacs could find the execuable
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
-
-(require 'hungry-delete)
-(global-hungry-delete-mode)
 (global-auto-revert-mode t)
-(require 'smartparens-config)
-;;(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
-(smartparens-global-mode t)
 
-;; config js2-mode for js files
-(setq auto-mode-alist
-	  (append
-	   '(("\\.js\\'" . js2-mode))
-	   auto-mode-alist))
-
-
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
 (global-set-key "\C-s" 'swiper)
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
 ;;(global-set-key (kbd "M-x") 'counsel-M-x)
 ;;(global-set-key (kbd "C-x C-f") 'counsel-find-file)
 ;;(global-set-key (kbd "C-h f") 'counsel-describe-function)
 ;;(global-set-key (kbd "C-h v") 'counsel-describe-variable)
-
 
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
@@ -75,13 +27,19 @@
 
 (global-linum-mode t)
 
+(abbrev-mode t)
+(define-abbrev-table 'global-abbrev-table '(
+					    ("8ms" "missnote")
+					    ))
+
+
 (defun open-my-init-file()
   (interactive)
   (find-file "~/.emacs.d/init.el"))
 
 (global-set-key (kbd "<f2>") 'open-my-init-file)
 
-(global-company-mode t)
+
 (setq-default cursor-type 'bar)
 
 (setq make-backup-files nil)
@@ -105,7 +63,7 @@
 
 (global-hl-line-mode t)
 
-(load-theme 'monokai t)
+
 
 (global-set-key (kbd "C-h C-f") 'find-function)
 (global-set-key (kbd "C-h C-v") 'find-variable)
@@ -113,6 +71,8 @@
 
  (setq org-agenda-files '("~/org"))
   (global-set-key (kbd "C-c a") 'org-agenda)
+
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
