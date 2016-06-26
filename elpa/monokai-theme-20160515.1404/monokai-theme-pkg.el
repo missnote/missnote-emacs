c310016cd91b0b0a2f9b42225513836660c94243
missnote <missnote.cn@gmail.com>
2016-06-27 07:57:30 +0800

    delete old packages

--- elpa/monokai-theme-20160515.1404/monokai-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "monokai-theme" "20160515.1404" "A fruity color theme for Emacs." 'nil :url "http://github.com/oneKelvinSmith/monokai-emacs")
--- elpa/web-mode-20160529.1148/web-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "web-mode" "20160529.1148" "major mode for editing web templates" 'nil :url "http://web-mode.org" :keywords '("languages"))
