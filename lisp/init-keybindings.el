63067fa4fe87c86d216b6e47bb3dfe90ae557e4b
missnote <missnote.cn@gmail.com>
2016-06-26 20:52:53 +0800

    delete miss.org

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -43,4 +43,10 @@
 
 (global-set-key (kbd "M-s e") 'iedit-mode)
 
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
+
 (provide 'init-keybindings)
